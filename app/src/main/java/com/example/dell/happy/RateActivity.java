a2161993ae2a7d49305dde78a00d62a6445427a2
dmfea <735418688@qq.com>
2019-04-17 17:17:18 +0800

    自定义汇率

--- app/src/main/java/com/example/dell/happy/RateActivity.java
+++ app/src/main/java/com/example/dell/happy/RateActivity.java
@@ -3,8 +3,11 @@ package com.example.dell.happy;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +16,9 @@ import android.widget.Toast;
 public class RateActivity extends AppCompatActivity {
     EditText rmb;
     TextView show;
+    private float dollarRate=0.1f;
+    private float euroRate=0.2f;
+    private float wonRate=0.3f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +40,11 @@ public class RateActivity extends AppCompatActivity {
             Toast.makeText(this,"请输入金额",Toast.LENGTH_SHORT).show();
         }
         if(btn.getId()==R.id.btn_dollar){
-            val = r*(1/6.7f);
+            val = r*dollarRate;
         }else if(btn.getId()==R.id.btn_euro){
-            val = r*(1/11.0f);
+            val = r*euroRate;
         }else{
-            val = r*500;
+            val = r*wonRate;
         }
         show.setText(String.valueOf(String.format("%.2f",val)));
     }
@@ -50,5 +56,52 @@ public class RateActivity extends AppCompatActivity {
         Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jd.com"));
         Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("Tel:17396249909"));
         startActivity(hello);
+        finish();
+    }
+    public void openOne2(View btn){
+        openConfig();
+
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i("rate","givedollar"+dollarRate);
+        Log.i("rate","giveeuro"+euroRate);
+        Log.i("rate","givewon"+wonRate);
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1&&resultCode==2){
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+
+            Log.i("save","onActivityResult:dollarRate="+dollarRate);
+            Log.i("save","onActivityResult:euroRate="+euroRate);
+            Log.i("save","onActivityResult:wonRate="+wonRate);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
